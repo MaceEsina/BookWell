d7b03cfb6690e2f4a326c6cd824be0154eb6f234
Svetlana Esina <magistresina@gmail.com>
2022-12-22 19:19:35 +0700

    auth fix

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
   <div id="app" class="app">
-    <router-view/>
+    <div v-if='$store.state.isLoading' class="loader">
+      <h1>BookWell</h1>
+    </div>
+    <router-view v-else />
   </div>
 </template>
 
 <script>
+import { storeUser, getUser } from '@/helpers/localStorage'
+import { getStart } from '@/api'
+
 export default {
   name: "App",
+  methods: {
+    auth(user) {
+      getStart(user)
+      .then(({ data }) => {
+        const { user } = data
+        if (user) {
+          storeUser(user)
+          this.$store.commit('setUser', user)
+        }
+        this.$store.commit('setLoading', false)
+      })
+      .catch(error => console.log('ERROR', error))
+    }
+  },
+  created() {
+    const user = getUser()
+    if (user) this.auth(user)
+    else this.$store.commit('setLoading', false)
+  }
 }
 </script>
